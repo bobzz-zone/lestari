c8379201bdf5e7da182377b876ac316314d9697c
bobzz-zone <bobzz.zone@gmail.com>
2022-08-25 02:23:38 +0700

    deposit

--- lestari/lestari/doctype/gold_invoice/gold_invoice.py
+++ lestari/lestari/doctype/gold_invoice/gold_invoice.py
@@ -25,3 +25,13 @@ def get_gold_rate(item_group,customer,customer_group):
 	if customer_group_rate and customer_group_rate[0]:
 		return {"nilai":customer_group_rate[0][0]}
 	return {"nilai":0}
+@frappe.whitelist(allow_guest=True)
+def get_gold_purchase_rate(item_group,customer,customer_group):
+	#check if customer has special rates
+	customer_rate=frappe.db.sql("""select nilai_beli from `tabCustomer Rates` where customer="{}" and item_group="{}" and valid_from<="{}"  """.format(customer,item_group,now_datetime()),as_list=1)
+	if customer_rate and customer_rate[0]:
+		return {"nilai":customer_rate[0][0]}
+	customer_group_rate=frappe.db.sql("""select nilai_beli from `tabCustomer Group Rates` where customer_group="{}" and item_group="{}" and valid_from<="{}"  """.format(customer_group,item_group,now_datetime()),as_list=1)
+	if customer_group_rate and customer_group_rate[0]:
+		return {"nilai":customer_group_rate[0][0]}
+	return {"nilai":0}
