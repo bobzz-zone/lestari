7fec2fafa7c0ffa369468ac6088e119cbfc8eb86
bobzz-zone <bobzz.zone@gmail.com>
2022-08-24 22:48:39 +0700

    Update gold_rates.py

--- lestari/lestari/doctype/gold_rates/gold_rates.py
+++ lestari/lestari/doctype/gold_rates/gold_rates.py
@@ -1,11 +1,12 @@
 # Copyright (c) 2022, DAS and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe.utils import now_datetime
 from frappe.model.document import Document
 
 class GoldRates(Document):
 	pass
 @frappe.whitelist():
 def get_latest_rates():
-	return frappe.db.sql("select nilai from `tabGold Rates` where date<=NOW() order by date desc",as_dict=True)[0]
+	return frappe.db.sql("select nilai from `tabGold Rates` where date<='{}' order by date desc".format(now_datetime()),as_dict=True)[0]
