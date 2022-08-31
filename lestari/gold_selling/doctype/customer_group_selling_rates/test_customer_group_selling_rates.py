ba80a0b27b40003b4d06266e82a16c0fd1a4f2a9
root <root@ip-172-26-11-113.ap-southeast-1.compute.internal>
2022-08-31 16:59:29 +0000

    Selling Structure

--- lestari/gold_selling/doctype/customer_group_selling_rates/test_customer_group_selling_rates.py
+++ lestari/gold_selling/doctype/customer_group_selling_rates/test_customer_group_selling_rates.py
@@ -4,5 +4,5 @@
 # import frappe
 import unittest
 
-class TestCustomerRates(unittest.TestCase):
+class TestCustomerGroupSellingRates(unittest.TestCase):
 	pass
--- lestari/gold_selling/doctype/customer_selling_rates/test_customer_selling_rates.py
+++ lestari/gold_selling/doctype/customer_selling_rates/test_customer_selling_rates.py
@@ -4,5 +4,5 @@
 # import frappe
 import unittest
 
-class TestCustomerGroupRates(unittest.TestCase):
+class TestCustomerSellingRates(unittest.TestCase):
 	pass
