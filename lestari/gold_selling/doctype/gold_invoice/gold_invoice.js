b3e7d71bdc15ff635466dccb888b236005b7c965
bobzz-zone <bobzz.zone@gmail.com>
2022-09-01 00:35:29 +0700

    Update gold_invoice.js

--- lestari/gold_selling/doctype/gold_invoice/gold_invoice.js
+++ lestari/gold_selling/doctype/gold_invoice/gold_invoice.js
@@ -14,6 +14,19 @@ frappe.ui.form.on('Gold Invoice', {
                 	}
                 })
 		}
+		if(frm.doc.docstatus > 0) {
+			cur_frm.add_custom_button(__('Accounting Ledger'), function() {
+				frappe.route_options = {
+					voucher_no: frm.doc.name,
+					from_date: frm.doc.posting_date,
+					to_date: moment(frm.doc.modified).format('YYYY-MM-DD'),
+					company: frm.doc.company,
+					group_by: "Group by Voucher (Consolidated)",
+					show_cancelled_entries: frm.doc.docstatus === 2
+				};
+				frappe.set_route("query-report", "General Ledger");
+			}, __("View"));
+		}
 	},
 	discount:function(frm){
 		if (!frm.doc.discount){
