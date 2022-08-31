9c0c70e6989961396ad6c654611777ea314e7b17
bobzz-zone <bobzz.zone@gmail.com>
2022-09-01 00:25:24 +0700

    Update gold_invoice.py

--- lestari/gold_selling/doctype/gold_invoice/gold_invoice.py
+++ lestari/gold_selling/doctype/gold_invoice/gold_invoice.py
@@ -1,6 +1,7 @@
 import frappe
 from frappe.utils import now_datetime
 from frappe.model.document import Document
+from erpnext.accounts.utils import get_account_currency, get_fiscal_years, validate_fiscal_year
 class GoldInvoice(Document):
 	def validate(self):
 		#total items
@@ -32,8 +33,9 @@ class GoldInvoice(Document):
 		piutang_idr = frappe.db.get_single_value('Gold Selling Settings', 'piutang_idr')
 		cost_center = frappe.db.get_single_value('Gold Selling Settings', 'cost_center')
 		gl_entries={}
+		fiscal_years = get_fiscal_years(self.posting_date, company=self.company)[0][0]
 		#1 untuk GL untuk piutang Gold
-		gl_entries[piutang_gold]={
+		gl_entries[piutang_gold]=frappe._dict({
 									"posting_date":self.posting_date,
 									"account":piutang_gold,
 									"party_type":"Customer",
@@ -50,17 +52,17 @@ class GoldInvoice(Document):
 									#"remarks":"",
 									"is_opening":"No",
 									"is_advance":"No",
-									#"fiscal_year":"2022",
+									"fiscal_year":fiscal_years,
 									"company":self.company,
 									"is_cancelled":0
-									}
+									})
 		#2 untuk GL untuk penjualan IDR
 		for row in self.items:
 			if row.income_account in gl_entries:
 				gl_entries[row.income_account]['credit']=gl_entries[row.income_account]['credit']+(row.amount*self.tutupan)
 				gl_entries[row.income_account]['credit_in_account_currency']=gl_entries[row.income_account]['credit']
 			else:
-				gl_entries[row.income_account]={
+				gl_entries[row.income_account]=frappe._dict({
 									"posting_date":self.posting_date,
 									"account":row.income_account,
 									"party_type":"",
@@ -77,10 +79,10 @@ class GoldInvoice(Document):
 									#"remarks":"",
 									"is_opening":"No",
 									"is_advance":"No",
-									#"fiscal_year":"2022",
+									"fiscal_year":fiscal_years,
 									"company":self.company,
 									"is_cancelled":0
-									}
+									})
 		#GL For Advance
 		nilai_selisih_kurs=0
 		for row in self.gold_invoice_advance:
@@ -93,7 +95,7 @@ class GoldInvoice(Document):
 				dsk=nilai_selisih_kurs
 			else:
 				csk=nilai_selisih_kurs
-			gl_entries[selisih_kurs]={
+			gl_entries[selisih_kurs]=frappe._dict({
 									"posting_date":self.posting_date,
 									"account":selisih_kurs,
 									"party_type":"",
@@ -110,10 +112,10 @@ class GoldInvoice(Document):
 									#"remarks":"",
 									"is_opening":"No",
 									"is_advance":"No",
-									#"fiscal_year":"2022",
+									"fiscal_year":fiscal_years,
 									"company":self.company,
 									"is_cancelled":0
-									}
+									})
 		gl_entries = merge_similar_entries(gl_entries)
 		return gl_entries
 	def make_gl_entries(self, gl_entries=None, from_repost=False):
@@ -156,6 +158,75 @@ class GoldInvoice(Document):
 			deposit=frappe.get_doc("Customer Deposit",row.customer_deposit)
 			if deposit.gold_left >=row.gold_allocated:
 				frappe.db.sql("""update `tabCustomer Deposit` set  gold_left=gold_left + {} where name="{}" """.format(row.gold_allocated,row.customer_deposit),as_list=1)
+
+	def get_gl_dict(self, args, account_currency=None, item=None):
+		"""this method populates the common properties of a gl entry record"""
+
+		posting_date = args.get("posting_date") or self.get("posting_date")
+		fiscal_years = get_fiscal_years(posting_date, company=self.company)
+		if len(fiscal_years) > 1:
+			frappe.throw(
+				_("Multiple fiscal years exist for the date {0}. Please set company in Fiscal Year").format(
+					formatdate(posting_date)
+				)
+			)
+		else:
+			fiscal_year = fiscal_years[0][0]
+
+		gl_dict = frappe._dict(
+			{
+				"company": self.company,
+				"posting_date": posting_date,
+				"fiscal_year": fiscal_year,
+				"voucher_type": self.doctype,
+				"voucher_no": self.name,
+				"remarks": self.get("remarks") or self.get("remark"),
+				"debit": 0,
+				"credit": 0,
+				"debit_in_account_currency": 0,
+				"credit_in_account_currency": 0,
+				"is_opening": self.get("is_opening") or "No",
+				"party_type": None,
+				"party": None,
+				"project": self.get("project"),
+				"post_net_value": args.get("post_net_value"),
+			}
+		)
+
+		accounting_dimensions = get_accounting_dimensions()
+		dimension_dict = frappe._dict()
+
+		for dimension in accounting_dimensions:
+			dimension_dict[dimension] = self.get(dimension)
+			if item and item.get(dimension):
+				dimension_dict[dimension] = item.get(dimension)
+
+		gl_dict.update(dimension_dict)
+		gl_dict.update(args)
+
+		if not account_currency:
+			account_currency = get_account_currency(gl_dict.account)
+
+		if gl_dict.account and self.doctype not in [
+			"Journal Entry",
+			"Period Closing Voucher",
+			"Payment Entry",
+			"Purchase Receipt",
+			"Purchase Invoice",
+			"Stock Entry",
+		]:
+			self.validate_account_currency(gl_dict.account, account_currency)
+
+		if gl_dict.account and self.doctype not in [
+			"Journal Entry",
+			"Period Closing Voucher",
+			"Payment Entry",
+		]:
+			set_balance_in_account_currency(
+				gl_dict, account_currency, self.get("conversion_rate"), self.company_currency
+			)
+
+		return gl_dict
 @frappe.whitelist(allow_guest=True)
 def get_gold_rate(category,customer,customer_group):
 	#check if customer has special rates
