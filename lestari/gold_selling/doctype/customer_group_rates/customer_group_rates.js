608f892a570715d9c14feda638e29039a7513d93
root <root@ip-172-26-11-113.ap-southeast-1.compute.internal>
2022-09-04 06:07:40 +0000

    aa

--- lestari/gold_selling/doctype/customer_group_rates/customer_group_rates.js
+++ lestari/gold_selling/doctype/customer_group_rates/customer_group_rates.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2022, DAS and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Customer Selling Rates', {
+frappe.ui.form.on('Customer Group Rates', {
 	// refresh: function(frm) {
 
 	// }
--- lestari/gold_selling/doctype/customer_rates/test_customer_rates.py
+++ lestari/gold_selling/doctype/customer_rates/test_customer_rates.py
@@ -4,5 +4,5 @@
 # import frappe
 import unittest
 
-class TestCustomerGroupSellingRates(unittest.TestCase):
+class TestCustomerRates(unittest.TestCase):
 	pass
