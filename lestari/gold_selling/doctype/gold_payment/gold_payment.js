c362abec2dba4e6977cb28ec789746c8a33e873f
bobzz-zone <bobzz.zone@gmail.com>
2022-09-13 14:47:57 +0700

    Update gold_payment.js

--- lestari/gold_selling/doctype/gold_payment/gold_payment.js
+++ lestari/gold_selling/doctype/gold_payment/gold_payment.js
@@ -15,7 +15,7 @@ frappe.ui.form.on('Gold Payment', {
     			return {
     				"filters": {
     					"docstatus":1,
-    					"status":"Unpaid",
+    					//"status":"Unpaid",
     					"customer":doc.customer
     				}
     			};
