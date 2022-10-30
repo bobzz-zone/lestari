5d521a19bf3046c70ba9e51f925162846958f629
bobzz-zone <bobzz.zone@gmail.com>
2022-10-30 22:09:17 +0700

    Update form_order.py

--- lestari/gold_selling/doctype/form_order/form_order.py
+++ lestari/gold_selling/doctype/form_order/form_order.py
@@ -1,10 +1,13 @@
 # Copyright (c) 2022, DAS and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class FormOrder(Document):
 	@frappe.whitelist()
 	def match_data(self):
-		frappe.msgprint("Hola")
+		#sort all item into grouped array of category
+		data={}
+		#for row in self.items:
+		#	if 
