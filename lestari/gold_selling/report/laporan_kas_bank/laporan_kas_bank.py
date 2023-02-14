2eef397579acfb20bf7ce469bd2effbf143eb936
root <root@ip-172-26-11-113.ap-southeast-1.compute.internal>
2023-02-14 05:53:37 +0000

    laporankasbank

--- lestari/gold_selling/report/laporan_kas_bank/laporan_kas_bank.py
+++ lestari/gold_selling/report/laporan_kas_bank/laporan_kas_bank.py
@@ -9,8 +9,17 @@ def execute(filters=None):
 	
 	columns = [
 	"Name:Link/GL Entry:150",
-	"Account:Link/Account:150",
+	"Keterangan:Data:150",
 	"Cost Center:Link/Cost Center:150",
+	"Remarks:Data:150",
+	"Debit:Link/Account:150",
+	"Credit:Link/Account:150",
+	"Masuk:Data:150",
+	"Keluar:Data:150",
+	"Saldo:Data:150",
+	"Proses:Data:150",
+	"Penyebab:Data:150",
+	"Dok No:Data:150",
 	]
 	conditional = []
 	# if filters.status:
@@ -22,13 +31,23 @@ def execute(filters=None):
 	if filters.from_date:
 		conditional.append("posting_date BETWEEN '{}' AND '{}'".format(filters.from_date,filters.to_date))
 	data = frappe.db.sql("""SELECT 
-	name,
-	account,
-	cost_center
-	# CONCAT(po.`currency`," ", FORMAT(poi.`rate`,2) ),
-	# CONCAT(po.`currency`," ", FORMAT(poi.`net_amount`,2) ),
-	# CONCAT(po.`currency`," ", FORMAT(po.`grand_total`,2) ),
-	FROM `tabGL Entry`
+	gl.name,
+	acc.account_name,
+	gl.cost_center,
+	gl.remarks,
+	gl.account,
+	gl.against,
+	# gl.debit_in_account_currency,
+	# gl.credit_in_account_currency
+	CONCAT(cur.`symbol`," ", FORMAT(gl.`debit_in_account_currency`,2) ),
+	CONCAT(cur.`symbol`," ", FORMAT(gl.`credit_in_account_currency`,2) ),
+	CONCAT(cur.`symbol`," ", FORMAT(0,2) ),
+	'',
+	gl.voucher_type,
+	gl.voucher_no
+	FROM `tabGL Entry` gl
+	JOIN `tabAccount` acc ON acc.name = gl.account
+	JOIN `tabCurrency` cur ON cur.name = gl.account_currency
 	WHERE 
  	{0}
 			 """.format(' AND '.join(conditional)))
