b38ca656323857e07294abec18b2bc9eeb456dbd
root <root@ip-172-26-11-113.ap-southeast-1.compute.internal>
2023-02-14 04:39:45 +0000

    prepurchase

--- lestari/gold_selling/report/laporan_kas_bank/laporan_kas_bank.js
+++ lestari/gold_selling/report/laporan_kas_bank/laporan_kas_bank.js
@@ -4,6 +4,24 @@
 
 frappe.query_reports["Laporan Kas Bank"] = {
 	"filters": [
-
+		{
+		"fieldname":"name",
+			"label": __("Name"),
+			"fieldtype": "Link",
+			"options": "GL Entry",
+		},{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			// "default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -24),
+			"reqd": 1
+		},{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"reqd": 1
+		},
 	]
 };
--- lestari/gold_selling/report/laporan_kas_bank/laporan_kas_bank.py
+++ lestari/gold_selling/report/laporan_kas_bank/laporan_kas_bank.py
@@ -1,9 +1,35 @@
-# Copyright (c) 2023, DAS and contributors
+# Copyright (c) 2013, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
-
+from __future__ import unicode_literals
+import frappe
 
 def execute(filters=None):
 	columns, data = [], []
+	
+	columns = [
+	"Name:Link/GL Entry:150",
+	"Account:Link/Account:150",
+	"Cost Center:Link/Cost Center:150",
+	]
+	conditional = []
+	# if filters.status:
+	# 	conditional.append("po.status = '{}'".format(filters.status))
+	# if filters.id: 
+	# 	conditional.append("po.name = '{}'".format(filters.id))
+	# if filters.supplier:
+	# 	conditional.append("po.supplier = '{}'".format(filters.supplier))
+	if filters.from_date:
+		conditional.append("posting_date BETWEEN '{}' AND '{}'".format(filters.from_date,filters.to_date))
+	data = frappe.db.sql("""SELECT 
+	name,
+	account,
+	cost_center
+	# CONCAT(po.`currency`," ", FORMAT(poi.`rate`,2) ),
+	# CONCAT(po.`currency`," ", FORMAT(poi.`net_amount`,2) ),
+	# CONCAT(po.`currency`," ", FORMAT(po.`grand_total`,2) ),
+	FROM `tabGL Entry`
+	WHERE 
+ 	{0}
+			 """.format(' AND '.join(conditional)))
 	return columns, data
