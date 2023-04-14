6f015efc92a42a279c2f23387f320ccb5a6846b7
bobzz-zone <bobzz.zone@gmail.com>
2023-04-14 14:29:47 +0700

    Update gold_payment.js

--- lestari/gold_selling/doctype/gold_payment/gold_payment.js
+++ lestari/gold_selling/doctype/gold_payment/gold_payment.js
@@ -25,8 +25,10 @@ function refresh_total_and_charges(frm){
 	frm.doc.total_extra_charges=frm.doc.write_off+ frm.doc.total_biaya_tambahan - frm.doc.bonus - frm.doc.discount_amount;
 	refresh_field("total_extra_charges");
 	frm.doc.total_sisa_invoice=frm.doc.total_invoice + frm.doc.total_extra_charges - frm.doc.allocated_payment;
-	if (frm.doc.write_off<0){
-		frm.doc.total_sisa_invoice=frm.doc.total_sisa_invoice-frm.doc.write_off;
+	if (frm.doc.total_sisa_invoice<0){
+		frm.doc.unallocated_payment=frm.doc.total_sisa_invoice*-1;
+		refresh_field("unallocated_payment");
+		frm.doc.total_sisa_invoice=0;
 	}
 	refresh_field("total_sisa_invoice");
 }
