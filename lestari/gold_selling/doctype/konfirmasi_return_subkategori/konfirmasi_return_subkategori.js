4520b1a9688d42afdcd47d2d1b8beb2a8bdb330b
bobzz.zone <bobzz.zone@gmail.com>
2023-04-19 04:10:21 +0000

    timbangan

---
+++ lestari/gold_selling/doctype/konfirmasi_return_subkategori/konfirmasi_return_subkategori.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, DAS and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Konfirmasi Return Subkategori', {
+	// refresh: function(frm) {
+
+	// }
+});
