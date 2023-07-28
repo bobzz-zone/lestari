f0f9d7a3f62a79be1e55aedc4bd02e98f109daef
bobzz.zone <bobzz.zone@gmail.com>
2023-07-28 16:36:14 +0700

    spkolilin

--- lestari/lestari/doctype/spko_inject_lilin/spko_inject_lilin.js
+++ lestari/lestari/doctype/spko_inject_lilin/spko_inject_lilin.js
@@ -19,12 +19,19 @@ frappe.ui.form.on('SPKO Inject Lilin', {
 			method: "lestari.lestari.doctype.spko_inject_lilin.spko_inject_lilin.get_items_from_rph_lilin",
 			source_doctype: "RPH Lilin",
 			// doctype: "Form Order",
-			// doctype: "RPH Lilin",
+			// doctype: "RPH Lilin Detail",
 			target: frm,
 			setters: {
 			//   name: me.frm.doc.rph_lilin
+				// // parent: undefined,
+				// no_spk: undefined,
+				// kadar: undefined,
+				// kategori : undefined,
+				// sub_kategori : undefined,
+				// qty: undefined
 			},
 			add_filters_group: 1,
+			// columns: ["no_spk", "kadar", "kategori","sub_kategori","qty"],
 			size: "extra-large",
 			get_query_filters: {
 			  docstatus: 1,
@@ -32,6 +39,17 @@ frappe.ui.form.on('SPKO Inject Lilin', {
 			  name: frm.doc.rph_lilin,
 			  kadar: frm.doc.kadar			  
 			},
+			// action(selections) {
+			// 	$.each(selections, function(i,g){
+			// 		// console.log("i"+i);
+			// 		// console.log("\ng"+g);
+			// 		cur_frm.add_child('items', {
+			// 			child_no: g,
+			// 		});
+					
+			// 	})
+			// 	cur_frm.refresh_field('items');
+			// }
 			allow_child_item_selection: true,
 			// child_fieldname: "items_valid",
       		// child_columns: ["model", "item_name", "kadar", "kategori", "sub_kategori", "kategori_pohon", "qty_isi_pohon", "no_pohon", "qty"],
