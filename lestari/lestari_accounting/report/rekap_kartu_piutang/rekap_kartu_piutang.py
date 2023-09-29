00f5db4b555fda2f0784b2486cacbafea84e8476
bobzz-zone <bobzz.zone@gmail.com>
2023-09-29 16:16:05 +0700

    Update rekap_kartu_piutang.py

--- lestari/lestari_accounting/report/rekap_kartu_piutang/rekap_kartu_piutang.py
+++ lestari/lestari_accounting/report/rekap_kartu_piutang/rekap_kartu_piutang.py
@@ -5,5 +5,20 @@
 
 
 def execute(filters=None):
-	columns, data = [], []
+	columns, data = ["Customer:Link/Customer:200","Deposit IDR:Currency:200","Total Piutang Emas:Currency:300"], []
+	data = frappe.db.sql("""select customer,sum(deposit_idr),sum(outstanding) from (SELECT
+            customer,
+            "0" AS deposit_idr,
+            outstanding
+            FROM
+            `tabGold Invoice`
+            WHERE docstatus = 1
+            UNION
+            SELECT
+            customer,
+            idr_left AS deposit_idr,
+            gold_left*-1 AS outstanding,
+            FROM
+            `tabCustomer Deposit`
+            WHERE docstatus = 1) a group by a. customer """, as_list=1)
 	return columns, data
