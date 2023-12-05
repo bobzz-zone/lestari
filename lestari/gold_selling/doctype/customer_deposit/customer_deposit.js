24b7fd8c8a1d0a086dca88dfb14a6412c077d32d
bobzz-zone <bobzz.zone@gmail.com>
2023-12-05 14:54:40 +0700

    Update customer_deposit.js

--- lestari/gold_selling/doctype/customer_deposit/customer_deposit.js
+++ lestari/gold_selling/doctype/customer_deposit/customer_deposit.js
@@ -201,10 +201,8 @@ frappe.ui.form.on('Stock Payment', {
 	},
 	amount_idr:function(frm,cdt,cdn) {
 	    var d=locals[cdt][cdn];
-		d.bruto = Math.floor(d.amount_idr/frm.doc.tutupan)*100/d.rate;
-		d.amount= Math.floor(d.amount_idr/frm.doc.tutupan);
-		refresh_field("bruto");
-		refresh_field("amount");
+		frappe.model.set_value(cdt, cdn,"bruto",Math.floor(d.amount_idr/frm.doc.tutupan)*100/d.rate);
+		frappe.model.set_value(cdt, cdn,"amount",Math.floor(d.amount_idr/frm.doc.tutupan));
 	}
 	
 });
