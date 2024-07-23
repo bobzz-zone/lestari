c465d88066f18e21d2bba8db14e040e045c154c9
bobzz-zone <bobzz.zone@gmail.com>
2024-07-23 12:31:37 +0700

    Update gold_payment.js

--- lestari/gold_selling/doctype/gold_payment/gold_payment.js
+++ lestari/gold_selling/doctype/gold_payment/gold_payment.js
@@ -508,17 +508,18 @@ frappe.ui.form.on('Gold Payment', {
 					//idr_emptied=true;
 				}*/
 				if (frm.doc.total_gold>0 && frm.doc.unallocated_payment>0){
-					frappe.msgprint("here");
 					for (var i=frm.doc.gold_invoice_advance.length;i>0 && gold_need_to_deduct>0;i--){
 						row = frm.doc.gold_invoice_advance[i-1];
+						frappe.msgprint("A");
 						if(row.gold_allocated>0){
+							frappe.msgprint("B");
 							if(gold_need_to_deduct > row.gold_allocated){
 								gold_need_to_deduct=gold_need_to_deduct-row.gold_allocated;
 								row.gold_allocated=0;
-								frappe.msgprint("Deducted to "+row.customer_deposit);
+								frappe.msgprint("Deducted 1");
 							}else{
 								row.gold_allocated=row.gold_allocated-gold_need_to_deduct;
-								frappe.msgprint("Deducted To "+row.customer_deposit);
+								frappe.msgprint("Deducted 2");
 								gold_need_to_deduct=0;
 							}
 						}
