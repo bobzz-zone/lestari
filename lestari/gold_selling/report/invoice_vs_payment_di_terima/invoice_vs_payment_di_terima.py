2eef36bbaf54d64dd8ce53a080cd4fdb4331fd74
bobzz-zone <bobzz.zone@gmail.com>
2024-08-06 13:11:48 +0700

    Update invoice_vs_payment_di_terima.py

--- lestari/gold_selling/report/invoice_vs_payment_di_terima/invoice_vs_payment_di_terima.py
+++ lestari/gold_selling/report/invoice_vs_payment_di_terima/invoice_vs_payment_di_terima.py
@@ -1,15 +1,23 @@
 # Copyright (c) 2024, DAS and contributors
 # For license information, please see license.txt
 
-# import frappe
-
+import frappe
+from frappe.utils import flt
 
 def execute(filters=None):
 	columns, data = [], []
+	columns= ["Posting Date:Date:150","Voucher Type:Data:100","Voucher No:Data:100","Item:Data:125","Qty:Float:100","Rate:Float:100","24K:Float:150","Balance:Float:150"]
 	#saldo awal
-
+	saldo_awal=frappe.db.sql("""select sum(netto) as total from `tabGold Log` where customer="{}" and date < "{}" group by customer """.format(filters.get("customer"),filters.get("from_date")),as_list=1)
+	saldo=0
+	if saldo_awal:
+		saldo=flt(saldo_awal[0][0])
+		data.append(["","","","Saldo Awal","","","",saldo])
 	#mutasi
 	log_data = frappe.db.sql("""select date, voucher_type,voucher_no , item,bruto,rate,netto from `tabGold Log` 
 		where customer="{}" and date <= "{}" and date >="{}"
 	 """.format(filters.get("customer"),filters.get("to_date"),filters.get("from_date")))
+	for row in log_data:
+		saldo=saldo+flt(row[6])
+		data.append([row[0],row[1],row[2],row[3],row[4],row[5],row[6],saldo])
 	return columns, data
