6f556daaf8ab29fe4e8bcc6829873102d047c39a
bobzz-zone <bobzz.zone@gmail.com>
2024-08-13 15:46:08 +0700

    Update pengembalian_deposit.js

--- lestari/gold_selling/doctype/pengembalian_deposit/pengembalian_deposit.js
+++ lestari/gold_selling/doctype/pengembalian_deposit/pengembalian_deposit.js
@@ -18,7 +18,7 @@ frappe.ui.form.on('Pengembalian Deposit', {
 			cur_frm.add_custom_button(__('Accounting Ledger'), function() {
 				frappe.route_options = {
 					voucher_no: frm.doc.name,
-					from_date: frm.doc.posting_date,
+					from_date: frm.doc.date,
 					to_date: moment(frm.doc.modified).format('YYYY-MM-DD'),
 					company: frm.doc.company,
 					group_by: "Group by Voucher (Consolidated)",
